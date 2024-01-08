d16ba641481407ce3dad2ee458eeb1d8ea24c7dc
Joan254 <joanotieno1998@gmail.com>
2024-01-08 10:40:00 +0000

    Automating the task of creating a custom HTTP header response using Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Automating the task of creating a custom HTTP header response using Puppet
+exec { 'command':
+   command  => 'sudo apt-get -y update;
+   sudo apt-get -y install nginx;
+   sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+   sudo service nginx restart',
+   provider => shell,
+}
