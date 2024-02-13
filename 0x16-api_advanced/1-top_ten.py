965cf5e7794a508a273dcf9e2aa03b606c38e75f
Joan254 <joanotieno1998@gmail.com>
2024-02-13 11:28:16 +0000

    Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,11 +12,12 @@ def top_ten(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'api_advanced-project'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
+    response = get(url, headers=user_agent, params=params,
+                   allow_redirects=False)
     results = response.json()
 
     try:
