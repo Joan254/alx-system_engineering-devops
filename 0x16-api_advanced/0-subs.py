975d9a08073ed0d25496a520520470090e08e2a5
Joan254 <joanotieno1998@gmail.com>
2024-02-13 10:54:31 +0000

    Function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,14 +12,13 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'linux:0x16.api.advanced:v1.0.0 (by Joan)'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
+    results = response.json()
 
-    if response.status_code == 200:
-        results = response.json()
-        return results.get("data", {}).get("subscribers", 0)
-    elif response.status_code == 404:
-        return 0
-    else:
-        return 0
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0 
